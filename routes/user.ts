34bce54eadfca4d9e96f4ae011606006e129b680
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2022-06-04 17:28:52 -0300

    Cambio de nombre a las rutas

--- routes/user.ts
+++ routes/user.ts
@@ -8,9 +8,9 @@ const router = Router()
 
 router.get('/',getUsers)
 router.get('/:id',getUser)
-router.post('/',postUser)
-router.put('/:id',putUser)
-router.delete('/:id',deleteUser)
+router.post('/create',postUser)
+router.put('/modify/:id',putUser)
+router.delete('/delete/:id',deleteUser)
 
 
 export default router
