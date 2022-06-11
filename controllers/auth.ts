5ab35d7897c13a99c5023ed0fa5cd404ce5b2945
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2022-06-11 20:29:09 -0300

    validar contraseñas

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,11 +1,37 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
+import { checkIfEmailExists } from '../helpers/check_if_params_exists'
+import bcryptjs from 'bcryptjs'
+import User from '../models/user'
 
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  try {
+    if (!(await checkIfEmailExists(email))) {
+      return res.status(404).json({
+        msg: `No existe usuario con email: ${email}`,
+      })
+    }
+    const user = await User.findOne({ where: { email: email } })
 
+    const isValidPassword = bcryptjs.compareSync(password, user.password)
 
-export const login = (req: Request, res: Response) => {
+    if (isValidPassword) res.json({
+        msg: 'Contraseña valida',
+        email,
+        password,
+      })
 
-    res.json({
-        msg: 'Login ok'
+    else res.status(400).json({
+      msg: 'Contraseña invalida',
+      email,
+      password,
     })
-}
 
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      msg: 'Ocurrio un error',
+    })
+  }
+}
--- dist/controllers/auth.js
+++ dist/controllers/auth.js
@@ -1,10 +1,50 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.login = void 0;
-const login = (req, res) => {
-    res.json({
-        msg: 'Login ok'
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.login = void 0;
+const check_if_params_exists_1 = require("../helpers/check_if_params_exists");
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
+const user_1 = __importDefault(require("../models/user"));
+const login = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password } = req.body;
+    try {
+        if (!(yield (0, check_if_params_exists_1.checkIfEmailExists)(email))) {
+            return res.status(404).json({
+                msg: `No existe usuario con email: ${email}`,
+            });
+        }
+        const user = yield user_1.default.findOne({ where: { email: email } });
+        const isValidPassword = bcryptjs_1.default.compareSync(password, user.password);
+        if (isValidPassword)
+            res.json({
+                msg: 'Contraseña valida',
+                email,
+                password,
+            });
+        else
+            res.status(400).json({
+                msg: 'Contraseña invalida',
+                email,
+                password,
+            });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Ocurrio un error',
+        });
+    }
+});
 exports.login = login;
 //# sourceMappingURL=auth.js.map
